3a7eb262483ef904d34ed019c665fd1cfefe5491
Weronika Motyl <weronika.motyl@comarch.com>
2025-06-10 11:25:50 +0200

    Initial commit

---
+++ app/src/main/java/com/dev/lamariposa/boardgamesassociation/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.dev.lamariposa.boardgamesassociation.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
